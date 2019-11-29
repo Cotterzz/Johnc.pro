262e34b243c5afbcbec5116e01273c76878d8cbe
John Cotterell <Cotterzz@users.noreply.github.com>
2019-11-29 11:10:37 +0000

    Update Foot.js

--- clarks/js/Foot.js
+++ clarks/js/Foot.js
@@ -2,9 +2,9 @@ class Foot extends THREE.Object3D{
 	constructor(){
 		super();
 		this.mesh = null;
-		var loader = new THREE.STLLoader();
+		var loader = new THREE.GLTFLoader();
 
-		loader.load( '/models/left.stl',  ( geometry ) => {
+		loader.load( '/models/left.glb',  ( geometry ) => {
      		this.mesh = new THREE.Mesh( geometry );
      		this.add(this.mesh);
      		console.log("LOADED:", geometry)
