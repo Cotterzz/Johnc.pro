177f378e96f0e081b6cc477b67c0995f309a3adc
John Cotterell <Cotterzz@users.noreply.github.com>
2019-11-29 10:20:59 +0000

    Added clarks project

---
+++ clarks/js/Foot.js
@@ -0,0 +1,26 @@
+class Foot extends THREE.Object3D{
+	constructor(){
+		super();
+		var loader = new THREE.STLLoader();
+
+		loader.load( './models/left.stl',  ( geometry ) => {
+     		this.add( new THREE.Mesh( geometry ) );
+   		})
+
+		//loader.parse( gltfData, null, ( gdata ) => {
+		//	this.add(gdata.scene);
+		//	this.headShape=gdata.scene
+		//	this.headShape.scale.x = this.headShape.scale.y = this.headShape.scale.z = 8;
+		//	this.headShape.rotation.x = 3.14159
+		//	this.headShape.rotation.z = 3.14159
+		//	//console.log(this.headShape.children[2])
+		//	this.headShape.children[2].material.color.set(this.skinColor);
+		//	this.headShape.children[2].material.metalness = 0.2;
+		//	this.headShape.children[2].material.roughness = 0.7;
+		//	this.headShape.castShadow = true;
+		//	this.castShadow = true;
+		//	this.headShape.children[2].castShadow = true;
+		//});
+
+	}
+}
---
+++ clarks/js/data.js
@@ -0,0 +1,46 @@
+var data = { 
+	"nodes": [{
+		"title":"FOOT DATA",
+		"type":"none",
+		"x":0,
+		"y":10,
+		"z":0,
+		"nodes":[
+		{
+			"title":"LEFT",
+			"type":"foot",
+			"x":-15,
+			"y":-10,
+			"z":3,
+			"nodes":[
+				{
+				"title":"Low Resolution Stl (1000 vertices)",
+				"type":"none",
+				"x":-10,
+				"y":-10,
+				"z":3,
+				"nodes":[]
+				}
+			]
+		},
+		{
+			"title":"RIGHT",
+			"type":"foot",
+			"x":15,
+			"y":-10,
+			"z":3,
+			"nodes":[
+				{
+				"title":"Original Resolution Stl (5000 vertices)",
+				"type":"none",
+				"x":10,
+				"y":-10,
+				"z":3,
+				"nodes":[]
+				}
+			]
+		}]}],
+	"joins": [
+    	//{"title": "none","nodeA": "FOOT DATA.LEFT","nodeB": "FOOT DATA.RIGHT","thickness": 0.5}
+  	]
+}
